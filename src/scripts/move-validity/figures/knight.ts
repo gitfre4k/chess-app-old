fc5f6ef1cfd1bb58be6946a82031a8aa34c04e2b
gitfre4k <zatocilo@gmail.com>
2022-02-05 16:07:22 +0100

    fix bug in knight logic

--- src/scripts/move-validity/figures/knight.ts
+++ src/scripts/move-validity/figures/knight.ts
@@ -1,5 +1,16 @@
 const knight = (pos1: number, pos2: number) => {
-  return [6, 10, 15, 17].includes(Math.abs(pos1 - pos2));
+  switch (pos1 % 8) {
+    case 0:
+      return [-6, -15, 10, 17].includes(pos1 - pos2);
+    case 7:
+      return [10, 15, 17, -6, -15, -17].includes(pos1 - pos2);
+    case 1:
+      return [6, 15, -10, -17].includes(pos1 - pos2);
+    case 2:
+      return [-10, -15, -17, 6, 15, 17].includes(pos1 - pos2);
+    default:
+      return [6, 10, 15, 17, -6, -10, -15, -17].includes(pos1 - pos2);
+  }
 };
 
 export default knight;
